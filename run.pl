17ccd71519ebaca1a5d9b5879931cff21e8d8d1e
Shin Okumura <s.okumura@iaea.org>
2022-03-23 20:22:20 +0100

    the decayfinite format added

--- run.pl
+++ run.pl
@@ -19,12 +19,14 @@ my $topdir  = "FY/";
 my $endff   = "/files/";
 my $tables  = "/tables/FY/";
 my $info    = "/tables/info/";
+my $decayfinite  = "decayfinite/";
 my @fymt    = ("454", "459");
 my $libname = undef;
 my @files   = ();
 my $outdir_tables = undef;
 my $outdir_info   = undef;
 my $outdir_files  = undef;
+my $outdir_decayfinite = undef;
 
 
 #=pod
@@ -57,6 +59,7 @@ foreach my $file (@files){
     $outdir_tables = &create_dir($type, $el, $a, $libname, $tables);
     $outdir_info   = &create_dir($type, $el, $a, $libname, $info);
     $outdir_files  = &create_dir($type, $el, $a, $libname, $endff);
+    $outdir_decayfinite = &create_dir($decayfinite, $el, $a, $libname, "");
 
     # run DeCE and output tabulated file
     &run_dece_table($file, $type, $el, $a, $libname, $outdir_tables);
@@ -68,38 +71,55 @@ foreach my $file (@files){
 
 #---------------------------------------------------------------------------
 #
-#  JENDL4.0 in jendl40-or-up-fy_20120914
+#  JENDL4.0/5.0 in jendl40-or-up-fy_20120914
 #
-$libname  = "jendl4.0";
-@files    =  glob "download/jendl40-or-up-fy_20120914/*.dat";
+my @libs = ("jendl4.0", "jendl5.0");
+# $libname  = "jendl4.0";
 
-foreach my $file (@files){
-    my $base = basename($file);
-    print "$base\n";
-    if ($base =~ /NF/){
-	$type = "n";
-    }
-    elsif ($base =~ /SF/){
-	$type = "0";
-    }
+foreach (@libs){
 
-    $base =~ /([a-zA-Z]{1,2})([0-9m]{3,4})/;
-    my $el = $1;
-    my $a  = $2;
+    if ($_ eq "jendl4.0") {$libname  = "jendl4.0"; @files = glob "download/jendl40-or-up-fy_20120914/*.dat";}
+    if ($_ eq "jendl5.0") {$libname  = "jendl5.0"; @files = glob "download/jendl5-fpy_upd2/*.dat";}
 
-    print "$libname, $type, $el, $a \n";
+    foreach my $file (@files){
+        my $el = "";
+        my $a = 0;
 
-    # create directory, delete all if exist 
-    $outdir_tables = &create_dir($type, $el, $a, $libname, $tables);
-    $outdir_info   = &create_dir($type, $el, $a, $libname, $info);
-    $outdir_files  = &create_dir($type, $el, $a, $libname, $endff);
-    print "$outdir_files\n";
+        my $base = basename($file);
+        print "$base\n";
+        if ($base =~ /NF|nf/){
+        $type = "n";
+        }
+        elsif ($base =~ /SF|sf/){
+        $type = "0";
+        }
 
-    # run DeCE and output tabulated file
-    &run_dece_table($file, $type, $el, $a, $libname, $outdir_tables);
-    &run_dece_info($file, $type, $el, $a, $libname, $outdir_info);
-    &cp_files($file, $type, $el, $a, $libname, $outdir_files);
-    &inc_energy_split($type, $el, $a, $libname, $outdir_tables);
+        if ($libname eq "jendl4.0") {
+            $base =~ /([a-zA-Z]{1,2})([0-9m]{3,4})/;
+            $el = $1;
+            $a  = $2;
+        }
+        if ($libname eq "jendl5.0") {
+            $base =~ /([0-9]{3})-([a-zA-Z]{1,2})-([0-9m]{3,4})/;
+            $el = $2;
+            $a  = $3;
+        }
+
+        print "$libname, $type, $el, $a \n";
+
+        # create directory, delete all if exist 
+        $outdir_tables = &create_dir($type, $el, $a, $libname, $tables);
+        $outdir_info   = &create_dir($type, $el, $a, $libname, $info);
+        $outdir_files  = &create_dir($type, $el, $a, $libname, $endff);
+        $outdir_decayfinite = &create_dir($decayfinite, $el, $a, $libname, "");
+        print "$outdir_files\n";
+
+        # run DeCE and output tabulated file
+        &run_dece_table($file, $type, $el, $a, $libname, $outdir_tables);
+        &run_dece_info($file, $type, $el, $a, $libname, $outdir_info);
+        &cp_files($file, $type, $el, $a, $libname, $outdir_files);
+        &inc_energy_split($type, $el, $a, $libname, $outdir_tables);
+        }
 }
 
 
@@ -132,6 +152,7 @@ foreach my $file (@files){
     $outdir_tables = &create_dir($type, $el, $a, $libname, $tables);
     $outdir_info   = &create_dir($type, $el, $a, $libname, $info);
     $outdir_files  = &create_dir($type, $el, $a, $libname, $endff);
+    $outdir_decayfinite = &create_dir($decayfinite, $el, $a, $libname, "");
 
     # run DeCE and output tabulated file
     &run_dece_table($file, $type, $el, $a, $libname, $outdir_tables);
@@ -141,16 +162,20 @@ foreach my $file (@files){
 }
 #=cut
 
+
+
 #---------------------------------------------------------------------------
 #
 #  sub
 #
+
+
 sub create_dir{
     my ($type, $el, $a, $libname, $ti) = @_;
     my $outdir  = $topdir . $type . "/" . $el . $a . "/" . $libname . $ti;
 
     if (-d $outdir){
-	# rmtree($outdir);  # caution!
+	rmtree($outdir);  # caution!
 	mkpath($outdir);
     } 
     else {
@@ -271,20 +296,110 @@ sub jeff_split{
     }
 }
 
-
-
 sub rtrim {
     my $val = shift;
     $val =~ s/\s+$//;
     return $val;
 }
 
+my @elem_list=("", 
+	   "H" , "He", "Li", "Be", "B" , "C" , "N" , "O" , "F" , "Ne",
+	   "Na", "Mg", "Al", "Si", "P" , "S" , "Cl", "Ar", "K" , "Ca",
+	   "Sc", "Ti", "V" , "Cr", "Mn", "Fe", "Co", "Ni", "Cu", "Zn",
+	   "Ga", "Ge", "As", "Se", "Br", "Kr", "Rb", "Sr", "Y" , "Zr",
+	   "Nb", "Mo", "Tc", "Ru", "Rh", "Pd", "Ag", "Cd", "In", "Sn",
+	   "Sb", "Te", "I" , "Xe", "Cs", "Ba", "La", "Ce", "Pr", "Nd",
+	   "Pm", "Sm", "Eu", "Gd", "Tb", "Dy", "Ho", "Er", "Tm", "Yb",
+	   "Lu", "Hf", "Ta", "W" , "Re", "Os", "Ir", "Pt", "Au", "Hg",
+	   "Tl", "Pb", "Bi", "Po", "At", "Rn", "Fr", "Ra", "Ac", "Th",
+	   "Pa", "U" , "Np", "Pu", "Am", "Cm", "Bk", "Cf", "Es", "Fm",
+	   "Md", "No", "Lr", "Rf", "Db", "Sg", "Bh", "Hs", "Mt", "Ds",
+	   "Rg", "Cn", "Nh", "Fl", "Mc", "Lv", "Ts", "Og", "Uue","Ubn",
+	   "Ubu","Ubb","Ubt","Ubq","Ubp","Ubh","Ubs","Ubo","Ube","Utn",
+	   "Utu","Utb","Utt","Utq","Utp","Uth","Uts","Uto","Ute","Uqn",);
+
+sub ztoelem {
+    my $z = shift;
+    my $elem_name = "";
+    if    ($z == "0")   {$elem_name = "g";}
+    else{
+	$elem_name = $elem_list[$z];
+    }
+    #print "$elem_list[$z]";
+    return $elem_name;
+}
+
+sub elemtoz {
+    my $elem_name = shift;
+    my $z = 0;
+    # $z =  grep { $elem_list[$_] eq $elem_name} 0..$#elem_list;
+    while ( my($indexnum, $elemm) = each @elem_list ) {
+	    if($elemm eq $elem_name){
+            $z = $indexnum;
+        }
+    }
+    print "$elem_name  --> $z\n";
+    return $z;
+}
+
+
 #---------------------------------------------------------------------------
 #
-#  Create Y(A) files
+#  Create decayfinite format files and Y(A) files
 #
+&make_decayfinite_ind();
+
 &make_ya_ind();
 &make_ya_cum();
+
+
+#  Create decayfinite format files
+sub make_decayfinite_ind{
+    # do not convert cumulative yield file
+    my @files = glob('FY/*/*/*/tables/FY/*[454]-E*');
+
+    foreach my $f (@files){
+        my @yeild = ();
+        my $basename = basename($f);
+        my $dirname  = dirname($f);
+        my @dirs = split(/\//, $dirname);
+
+        $basename =~ /[0n]-([A-Za-z]{1,2})([0-9m]{3,4})-MT454-E([0-9-+E.]{8})/;
+        print ("$1   $2    $3\n");
+        my $tar_el = $1;
+        my $tar_a  = $2;
+        my $e = $3;
+        my $tar_z = &elemtoz($tar_el);
+
+
+        my $decayfinitef = $topdir . $decayfinite . $dirs[2] . '/' . $dirs[3] . '/' . $basename;
+
+        print "$decayfinitef\n";
+        open (FINITE, "> $decayfinitef");
+
+        my $count = `cat $f | sed '\/\^\$\/\d' | wc -l`;
+
+
+        print  FINITE ("#  ", $basename, "  independent fission yields\n");
+        printf FINITE ("# %3d %5d %5d %5s %11.4E %11d\n", 0 ,$tar_z, $tar_a, $tar_z . "xx", $e, $count);
+
+        open(EN, "$f") or die "No file";
+        while(<EN>){
+            my $line  =  $_;
+            chomp $line;
+            if ($line =~ /^\s/){
+                $line =~ s/^\s+//;
+                my ($z, $a, $level, $fpy, $dfpy) = split(/\s+/, $line);
+                # print "$z, $a, $level, $fpy, $dfpy  \n"; #
+                my $n = $a - $z;
+                printf FINITE ("%5d %5d %5d %5d %18.6E %18.6E\n", $n ,$z ,$a ,$level, $fpy, $dfpy );
+            }
+        }
+    close(EN);
+    close(FINITE);
+    }
+}
+
 
 sub make_ya_ind{
     my @files = glob('FY/*/*/*/tables/FY/*[4]-E*');
@@ -293,7 +408,7 @@ sub make_ya_ind{
 	my @yeild = ();
 	my $basename = basename($f);
 	my $dirname  = dirname($f);
-	$basename =~ s/-E/-YA-E/;
+	$basename =~  s/(-E)([0-9])/-YA$1$2/;
 
 	open(EN, "$f") or die "No file";
     while(<EN>){
@@ -308,7 +423,7 @@ sub make_ya_ind{
     }
 
     my $outya = $dirname . '/' . $basename;
-    print "$outya\n";
+    # print "$outya\n";
     open (YA, "> $outya");
     my $i = "";
     for ($i=1; $i <=190; $i++){
@@ -324,7 +439,7 @@ sub make_ya_cum{
 	my @yeild = ();
 	my $basename = basename($f);
 	my $dirname  = dirname($f);
-	$basename =~ s/-E/-YA-E/;
+	$basename =~ s/(-E)([0-9])/-YA$1$2/;
 
 	open(EN, "$f") or die "No file";
     while(<EN>){
@@ -341,7 +456,7 @@ sub make_ya_cum{
     }
 	close(EN);
     my $outya = $dirname . '/' . $basename;
-    print "$outya\n";
+    # print "$outya\n";
     open (YA, "> $outya");
     my $i = "";
     for ($i=1; $i <=190; $i++){
@@ -349,3 +464,6 @@ sub make_ya_cum{
         }
     }
 }
+
+
+
